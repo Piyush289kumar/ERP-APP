2c00f4431270e45e05c060815397db649001acc9
Piyush289kumar <piyushraikwar289@gmail.com>
2025-10-23 13:01:03 +0530

    Create modifyPolicy

--- Backend/src/controllers/policy.controller.js
+++ Backend/src/controllers/policy.controller.js
@@ -38,3 +38,92 @@ export const getAllActivePolicies = async (req, res) => {
       .json({ message: "Internal Server Error", error: error.message });
   }
 };
+
+// Get Active Policy by Slug
+export const getActivePoliciesBySlug = async (req, res) => {
+  try {
+    const { slug } = req.params;
+    if (!slug) {
+      return res.status(400).json({ message: "Slug is required." });
+    }
+
+    const policy = await Policy.findOne({ slug }).lean();
+    if (!policy) {
+      return res
+        .status(404)
+        .json({ message: `Policy not found with slug : ${slug}` });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Policy fetched by slug successfully.", data: policy });
+  } catch (error) {
+    console.error("Error while fetch Active policy by slug.");
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+// Create new or Update Policy
+export const modifyPolicy = async (req, res) => {
+  try {
+    const {
+      id, // Optional for update
+      title,
+      slug,
+      description,
+      isActive,
+    } = req.body;
+
+    if (!title) {
+      return res.status(400).json({ message: "Policy Title is required." });
+    }
+
+    // If id exist then update
+    if (id) {
+      const existing = await Policy.findOne({ slug });
+      if (existing) {
+        return res
+          .status(400)
+          .json({ message: "Policy with this title is already exist." });
+      }
+
+      const policy = await Policy.findOne({ slug });
+      if (!policy) {
+        return res.status(404).json({ message: "Policy not found" });
+      }
+
+      // Update Fileds
+      policy.title = title || policy.title;
+      policy.slug = slug || policy.slug;
+      policy.description = description || policy.description;
+      policy.isActive = isActive ?? policy.isActive;
+      policy.updatedBy = req.user?._id;
+
+      await policy.save();
+      return res
+        .status(200)
+        .json({ message: "Policy updated successfully.", data: policy });
+    } else {
+      // Create New Record
+      const policy = await Policy.create({
+        title,
+        slug,
+        description,
+        isActive,
+        createdBy: req.user._id,
+      });
+
+      return res
+        .status(201)
+        .json({ message: "Policy created successfully.", data: policy });
+    }
+  } catch (error) {
+    console.error("Error while Modify Policy");
+
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
--- Backend/src/routes/policy.routes.js
+++ Backend/src/routes/policy.routes.js
@@ -1,7 +1,9 @@
 import Router from "express";
 import {
+  getActivePoliciesBySlug,
   getAllActivePolicies,
   getAllPolicies,
+  modifyPolicy,
 } from "../controllers/policy.controller.js";
 import { ensureAuth } from "../middleware/authMiddleware.js";
 
@@ -9,7 +11,9 @@ const router = Router();
 
 // Public Routes
 router.get("/", getAllActivePolicies);
+router.get("/view/:slug", getActivePoliciesBySlug);
 // Protected Routes
 router.get("/admin", ensureAuth, getAllPolicies);
+router.post("/admin/save", ensureAuth, modifyPolicy);
 
 export default router;
