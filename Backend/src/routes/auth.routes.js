1550654bcbb73796ffec29ab5cb0e590cadfb31a
Piyush289kumar <piyushraikwar289@gmail.com>
2025-10-25 16:25:12 +0530

    Create Upldate User Profile and add Avatar upload to Cludinary.

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
-import { getProfile, login, logout, register } from "../controllers/auth.controller.js";
+import { login, logout, register } from "../controllers/auth.controller.js";
 import { ensureAuth } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/profile", ensureAuth, getProfile);
 router.post("/logout", ensureAuth, logout);
 
 export default router;
